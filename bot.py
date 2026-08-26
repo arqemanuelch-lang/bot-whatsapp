be4b411dbd59eae06a21cd6573965345e82f0a58
arqemanuelch-lang <arq.emanuel.ch@gmail.com>
2026-08-26 15:51:16 -0300

    bot.py

--- bot.py
+++ bot.py
@@ -8,5 +8,29 @@ app = Flask(__name__)
 def home():
     return "¡El Bot está activo!", 200
 
+# Ruta para verificar y recibir mensajes de WhatsApp
+@app.route("/webhook", methods=["GET", "POST"])
+def webhook():
+    if request.method == "GET":
+        # Meta envía una petición GET para verificar el token
+        mode = request.args.get("hub.mode")
+        token = request.args.get("hub.verify_token")
+        challenge = request.args.get("hub.challenge")
+        
+        VERIFY_TOKEN = "miprimertoken123"
+        
+        if mode and token:
+            if mode == "subscribe" and token == VERIFY_TOKEN:
+                return challenge, 200
+            else:
+                return "Error de token", 403
+        return "Hola", 200
+
+    elif request.method == "POST":
+        # Aquí es donde el bot recibirá los mensajes de los usuarios más adelante
+        data = request.json
+        print(data)
+        return "EVENT_RECEIVED", 200
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
