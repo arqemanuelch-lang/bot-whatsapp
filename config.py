9bc38463a337a519b65f672e62b2273e0ce15df0
arqemanuelch-lang <arq.emanuel.ch@gmail.com>
2026-09-01 18:50:16 -0300

    config.py

--- config.py
+++ config.py
@@ -90,7 +90,8 @@ PRODUCTOS = {
     "kit_maestro": {
         "titulo": "Kit Maestro de Arquitectura y Construcción",
         "descripcion_corta": "8 manuales técnicos en PDF",
-        "precio": "$5.500",
+        "precio": "$8.000",
+        "precio_oferta": "$5.500",
         "link_pago": "https://mpago.la/17uyqFK",
         "imagen": "https://i.ibb.co/R40sSbHt/Sin-t-tulo-1080-1920-px-1080-x-1080-px-1080-x-1920-px-1080-x-1350-px-1.png",
         "imagen_oferta": "https://i.ibb.co/Z6NnT3sx/115829a6-72a0-425d-90d4-ba7ec17fcd78.jpg",
